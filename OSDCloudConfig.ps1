e25cc9666fdaeb04e4cf54c489aa5b6a61c0b53c
Advanced Business Systems <153853059+AdvancedBusinessSystems@users.noreply.github.com>
2025-03-26 11:44:42 -0400

    Update OSDCloudConfig.ps1

--- OSDCloudConfig.ps1
+++ OSDCloudConfig.ps1
@@ -1,7 +1,7 @@
 ﻿Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
 Start-Sleep -Seconds 5
 
-Start-OSDCloud -OSVersion 'Windows 10' -OSEdition Pro -OSLanguage en-us -OSLicense Retail -ZTI
+Start-OSDCloud -OSVersion 'Windows 11' -OSEdition Pro -OSLanguage en-us -OSLicense Retail -ZTI
 
 #Restart from WinPE
 
